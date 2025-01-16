519df8f7ef20252d197ad1ea0e1f4f158627adac
Bohdan Chokhlenko <105508224+den-ffv@users.noreply.github.com>
2025-01-16 21:24:04 +0200

    clear client

--- www/src/assets/formatData.ts
+++
@@ -1,9 +0,0 @@
-export const formatData = (dataString: string) => {
-    const data = new Date(dataString);
-
-    const day = String(data.getDate());
-    const month = String(data.getMonth() + 1).padStart(2, '0');
-    const year = data.getFullYear();
-
-    return `${day}-${month}-${year}`;
-}
