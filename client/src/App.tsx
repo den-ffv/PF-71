f312340fbf190e9a8920ee5e21052569a45f7ba6
Bohdan Chokhlenko <105508224+den-ffv@users.noreply.github.com>
2025-03-04 16:23:33 +0200

    add test web

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,33 +1,19 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { Routes, Route } from "react-router";
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <Routes>
+        <Route index element={<p>Home</p>} />
+        <Route path="about" element={<p>About</p>}/>
+
+        <Route path='auth' element={<p>LAST home</p>}>
+          <Route path="login" element={<p>login</p>}/>
+          <Route path="register" element={<p>register</p>}/>
+        </Route>
+      </Routes>
+
     </>
   )
 }
